eead73eede1a38174548a93877b0b982fbbd271f
Charles Yu <changxi.yu@gmail.com>
2012-02-11 02:22:46 +0800

    fix mistake

--- app.py
+++ app.py
@@ -148,7 +148,6 @@ application = SessionMw(app)
 
 if __name__ == '__main__':
     #bottle.debug(True)
-    '''from gevent import monkey
+    from gevent import monkey
     monkey.patch_all()
-    run(app, host='localhost', port=8000, server='gevent', reloader=True)'''
-    run(app, host='localhost', port=8000, reloader=True)
+    run(application, host='localhost', port=8000, server='gevent', reloader=True)
